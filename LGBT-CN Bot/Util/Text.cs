9159e82ee717c1d4d267875d0d8b80a75eb23283
Kevin CHEN <33132228+KevinZonda@users.noreply.github.com>
2021-01-22 23:58:36 +0000

    Update Text.cs

--- LGBT-CN Bot/Util/Text.cs
+++ LGBT-CN Bot/Util/Text.cs
@@ -5,12 +5,12 @@ namespace LGBTCN.Bot.Util
     static class Text
     {
         public const string USAGE = "NO USAGE AVAILABLE!";
-        public const string Message_IS_INVALID = "Message is valid! Text required!";
+        public const string Message_IS_INVALID = "Message is invalid! Text required!";
         public const string OOPS = "Oops! Something happened! Please contact the maintainer @LGBTCN";
         public static string ABOUT =
-            "LGBT-CN TRANSLATE BOT\n" +
-            "---------------------\n" +
-            "MAINTAINER: LGBT-CN COMMUNITY\n" +
+            $"LGBT-CN TRANSLATE BOT\n" +
+            $"---------------------\n" +
+            $"MAINTAINER: LGBT-CN COMMUNITY\n" +
             $"OS: {Environment.OSVersion}\n" +
             $"RUNTIME: .NET {Environment.Version}";
     }
