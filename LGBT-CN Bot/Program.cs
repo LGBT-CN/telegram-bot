0a11917a110a06f5510bd5690e12622b87142151
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2021-01-22 23:06:41 +0000

    add function

--- LGBT-CN Bot/Program.cs
+++ LGBT-CN Bot/Program.cs
@@ -16,6 +16,10 @@ namespace LGBTCN.Bot
     {
         private static TelegramBotClient _bot;
 
+        private static Translate _translateEN = new Translate(Translate.Language.Auto, Translate.Language.English);
+
+        private static Translate _translateZH = new Translate(Translate.Language.Auto, Translate.Language.Chinese);
+
         public static async Task Main()
         {
             _bot = new TelegramBotClient(Configuration.BotToken);
@@ -39,6 +43,17 @@ namespace LGBTCN.Bot
         private static async void BotOnMessageReceived(object sender, MessageEventArgs messageEventArgs)
         {
             var message = messageEventArgs.Message;
+
+            if (message.Chat.Username != Text.ALLOW_USERNAME && message.From.Id != Text.DEBUGGER_ID)
+            {
+                Log.E("main.msg", $"Not allowed group! {{\"id\":{message.Chat.Id}," +
+                    $"\"title\":\"{message.Chat.Title}\"," +
+                    $"\"username\":\"{message.Chat.Username}\"," +
+                    $"\"type\":\"{message.Chat.Type}\"," +
+                    $"}}");
+                return;
+            }
+
             if (message == null || message.Type != MessageType.Text)
                 return;
 
@@ -47,20 +62,94 @@ namespace LGBTCN.Bot
 
             switch (message.Text.Split(' ').First())
             {
+                case "/translate":
+                    await SendTranslate(message);
+                    break;
                 case "/start":
                     await SendMsg(message, "Hello");
+                    break;
+                case "/debug":
+                    await SendDebug(message);
+                    break;
+                case "/about":
+                    await SendMsg(message, Text.ABOUT);
                     break;
                 default:
                     await SendUsage(message);
                     break;
             }
         }
+
+        static async Task SendDebug(Message message)
+        {
+            string replyStr = "NO PERMISSION!";
+            if (message.From.Id == 573387497)
+            {
+                replyStr = $"CHAT.ID {message.Chat.Id}\n" +
+                    $"CHAT.UN {message.Chat.Username}\n" +
+                    $"CHAT.TTL {message.Chat.Title}";
+            }
+            await _bot.SendTextMessageAsync(
+                    replyToMessageId: message.MessageId,
+                    chatId: message.Chat.Id,
+                    text: replyStr,
+                    replyMarkup: new ReplyKeyboardRemove()
+                );
+            return;
+        }
+
+        static async Task SendTranslate(Message message)
+        {
+            string replyStr;
+            int replyMsgId;
+            if (message.ReplyToMessage == null)
+            {
+                replyStr = "The message shouldn't be empty!";
+                replyMsgId = message.MessageId;
+            }
+            else
+            {
+                var text = message.ReplyToMessage.Text;
+                replyStr = await Task.Run(() => GetTransMsg(text));
+                replyMsgId = message.ReplyToMessage.MessageId;
+            }
+            await _bot.SendTextMessageAsync(
+                    replyToMessageId: replyMsgId,
+                    chatId: message.Chat.Id,
+                    text: replyStr,
+                    replyMarkup: new ReplyKeyboardRemove()
+                );
+            return;
+        }
+
+        static string GetTransMsg(string text)
+        {
+            try
+            {
+                string result = $"Orgin: {text}\n";
+                text = text.Trim();
+                var en = _translateEN.Text(text).Trim();
+                var zh = _translateZH.Text(text).Trim();
+                if (text != en)
+                    result += "EN: " + en + "\n";
+                if (text != zh)
+                    result += "ZH: " + zh + "\n";
+                return result.Trim();
+            }
+            catch(Exception ex)
+            {
+                Log.E("main.translate", ex.ToString());
+                return Text.OOPS;
+            }
+        }
+
         static async Task SendUsage(Message message)
             => await SendMsg(message, Text.USAGE);
 
-        static async Task SendMsg(Message message, string text)
+        static async Task SendMsg(Message message, string text, int replyToMessageId = 0)
         {
             await _bot.SendTextMessageAsync(
+                replyToMessageId: replyToMessageId,
                 chatId: message.Chat.Id,
                 text: text,
                 replyMarkup: new ReplyKeyboardRemove()
@@ -69,7 +158,7 @@ namespace LGBTCN.Bot
         private static void BotOnReceiveError(object sender, ReceiveErrorEventArgs receiveErrorEventArgs)
         {
             Log.E("main.connect", $"{receiveErrorEventArgs.ApiRequestException.ErrorCode.ToString()}" +
-                  " — " +
+                  " - " +
                   $"{receiveErrorEventArgs.ApiRequestException.Message}");
         }
 
--- LGBT-CN Bot/Util/Text.cs
+++ LGBT-CN Bot/Util/Text.cs
@@ -1,13 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LGBTCN.Bot.Util
 {
     static class Text
     {
-        public const string USAGE = "";
+        public const string USAGE = "NO USAGE AVAILABLE!";
+        public const string Message_IS_INVALID = "Message is valid! Text required!";
+        public const int DEBUGGER_ID = 0;
+        public const string ALLOW_USERNAME = "LGBTCN";
+        public const string OOPS = "Oops! Something happened! Please contact the maintainer @LGBTCN";
+        public static string ABOUT =
+            "LGBT-CN TRANSLATE BOT\n" +
+            "---------------------\n" +
+            "MAINTAINER: LGBT-CN COMMUNITY\n" +
+            $"OS: {Environment.OSVersion}\n" +
+            $"RUNTIME: .NET {Environment.Version}";
     }
 }
